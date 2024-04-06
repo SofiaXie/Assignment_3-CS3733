d0d72eaf4cb60ff4765ff942680524d4ad6fe554
SofiaXie <sofalofax@gmail.com>
2024-04-06 17:54:02 -0400

    Added other selection items, included material UI, curently using code based off flower page to bea bel to connect to database.

--- src/ServiceRequest.tsx
+++ src/ServiceRequest.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import axios from "axios";
+import {Table} from "@mui/material";
 
 const ServiceRequest = () => {
     const [formState, setFormState] = useState({
@@ -8,11 +9,12 @@ const ServiceRequest = () => {
         requestPriority: "",
         location: "",
         status: "",
+        medicalDevice: "",
     });
 
     const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement| HTMLSelectElement | HTMLTextAreaElement>) => {
         setFormState({
             ...formState,
             [event.target.name]: event.target.value,
@@ -43,6 +45,7 @@ const ServiceRequest = () => {
             <div>
                 <h1> Enter Your Service Request </h1>
                 <form onSubmit={handleSubmit}>
+
                     {/*Ask for Name of Employee*/}
                     <div>
                         <label>Name of the employee</label>
@@ -54,37 +57,102 @@ const ServiceRequest = () => {
                                className={"form-control"}
                                required/>
                     </div>
+
                     {/* Request Priority*/}
                     <div>
-                        <label> Request Priority </label>
-                        <input
-                            type="text"
-                            name={"requestPriority"}
-                            placeholder={"Priority"}
-                            value={formState.requestPriority}
-                            onChange={handleChange}
-                            className={"form-control"}
-                            required/>
+                        <label>
+                            Priority:
+                            <select name="requestPriority" value={formState.requestPriority} onChange={handleChange}
+                                    className={"form-control"} required>
+                                <option value="">Select Priority</option>
+                                <option value="Low">Low</option>
+                                <option value="Medium">Medium</option>
+                                <option value="High">High</option>
+                                <option value="Emergency">Emergency</option>
+                            </select>
+                        </label>
                     </div>
+
+                    {/*Location in Building*/}
                     <div>
-                        <button type="submit">
-                            Send
+                        <label>
+                            Location:
+                            <input type="text" name="location" value={formState.location} onChange={handleChange}/>
+                        </label>
+                    </div>
+
+                    {/*Medical Device Selection*/}
+                    <div>
+                        <legend>Choose Medical Device:</legend>
+
+                        <div>
+                            <input type="checkbox" id="hospitalBeds" name="hospitalBeds"/>
+                            <label htmlFor="hospitalBeds">Hospital Bed</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="IVpumps" name="IVpumps"/>
+                            <label htmlFor="IVpumps">IV pumps</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="recliners" name="recliners"/>
+                            <label htmlFor="recliners">Recliners</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="Ventilators" name="Ventilators"/>
+                            <label htmlFor="Ventilators">Ventilators</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="Anesthesia" name="Anesthesia"/>
+                            <label htmlFor="Anesthesia">Anesthesia Machines</label>
+                        </div>
+
+                        <div>
+                            <input type="checkbox" id="Pacemakers" name="Pacemakers"/>
+                            <label htmlFor="Pacemakers">Pacemakers</label>
+                        </div>
+                    </div>
+
+                    {/*Request status*/}
+                    <div>
+                        <label>
+                            Status:
+                            <select name="status" value={formState.status} onChange={handleChange}>
+                                <option value="Unassigned">Unassigned</option>
+                                <option value="Assigned">Assigned</option>
+                                <option value="InProgress">InProgress</option>
+                                <option value="Closed">Closed</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    {/*Submit Button*/}
+                    <div>
+                        <button className={"actionButton"} type="submit">
+                            Send Request
                         </button>
                     </div>
+
                 </form>
             </div>
 
-        {/* Display Table*/}
+            {/* Display Table*/}
             <div>
-                <table>
+                <Table stickyHeader aria-label="sticky table" className={"contentTable"}>
                     <thead>
-                        <tr>
-                            <th>Employee Name</th>
-                            <th>Request Priority</th>
-                            <th>Location</th>
-                            <th>Status</th>
-                        </tr>
+                    <tr>
+                        <th>Employee Name</th>
+                        <th>Request Priority</th>
+                        <th>Location</th>
+                        <th>Medical Device</th>
+                        <th>Status</th>
+                    </tr>
+
                     </thead>
+
                     {/*uses an arry to store the data then displays it*/}
                     {/*<tbody>*/}
                     {/*{isSubmitted.map((data, index) => (*/}
@@ -95,7 +163,7 @@ const ServiceRequest = () => {
 
                     {/*))}*/}
 
-                </table>
+                </Table>
             </div>
         </>
     );
